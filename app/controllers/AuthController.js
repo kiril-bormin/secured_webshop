c6560fbc231a9cecd7fb2eb14006547bc1b6809a
Kiril Bormin <borminnkiril@gmail.com>
2026-04-24 12:11:17 +0200

    feat (mdp) : ajout d'une politique de mdp fort

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -66,8 +66,17 @@ module.exports = {
   register: (req, res) => {
     const { username, email, password } = req.body;
 
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
     if (!username || !email || !password) {
       return res.status(400).json({ error: "Tous les champs sont requis" });
+    }
+    if (!passwordRegex.test(password)) {
+      return res.status(400).json({
+        error:
+          "Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial.",
+      });
     }
 
     const passwordWithPepper = password + PEPPER;
